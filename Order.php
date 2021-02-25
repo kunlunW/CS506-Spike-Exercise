b33133dfdb21ff5ca83bb03b0d4a5fdaf5a9ec8c
EVEBios <61592111+EVEBios@users.noreply.github.com>
2021-02-25 10:39:43 +0800

    Update Order.php

--- Order.php
+++ Order.php
@@ -3,9 +3,27 @@ include "sql_functions.php"
 
 class Order
 {
-    function __construct()
+    private $username;
+    private $menuname;
+    private $quantity;
+    private $pickupdate;
+    private $orderID;
+    private $vehdesc;
+    private $priority;
+    
+    function __construct($userName, $menuname, $quantity, $pickupdate, $vehdesc)
     {
-
+        $this->username = $username;
+        $this->menuname = $menuname;
+        $this->quantity = $quantity;
+        $this->pickupdate = $pickupdate;
+        $this->vehdesc = $vehdesc;
+        
+        $conn = OpenCon();
+        $this->orderID = GetNextID($conn);
+        $user = FindOrder($conn, $this->username);
+        
+        AddOrder($this->conn, $this->orderID, $this->menuname, $this->quantity, $this->pickupdate, $this->vehdesc, $priority='5');
     }
 }
 ?>
