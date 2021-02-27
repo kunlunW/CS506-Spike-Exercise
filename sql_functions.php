17f51285b3a0b4fbb8e4f976f0c1724025cdc6bb
benjaminreas <56094366+benjaminreas@users.noreply.github.com>
2021-02-26 19:59:36 -0600

    Update sql_functions.php

--- sql_functions.php
+++ sql_functions.php
@@ -21,7 +21,7 @@ function CreateUsersTable($conn)
 {
     $sql = "CREATE TABLE users (
     username VARCHAR(255) PRIMARY KEY,
-    password VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     phonenumber CHAR(10),
     address VARCHAR(255),
     type ENUM('customer', 'admin', 'staff') DEFAULT 'customer',
